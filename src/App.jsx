7b47c7191b36557d8447470a4b6579b16b246d44
Khalil Ahmad <91117263+APRUN@users.noreply.github.com>
2024-06-15 05:55:11 +0000

    chnaged App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Khalil Ahmad",
+  title: "Web Developer & Designer",
+  email: "iamkhalil2005@gmail.com",
+  gitHub: "APRUN",
+  instagram: "khalilahmad.zip",
+  linkedIn: "khalil ahmad",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "Gaming Buzz",
 };
 
 const primaryColor = "#4E567E";
